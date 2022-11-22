4a57b9b041997f2291f551b18b2b37910122c5ce
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2022-11-22 21:47:07 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -5,5 +5,14 @@ int isArmstrong(int n);
 int isPalindrome(int n);
 int isPrime(int n);
 int isStrong(int n);
+int rec_fact_(int n);
+int length_(int n);
+int my_pow_(int n, int j);
+int ezer(int n,int l);
+int rec_fact(int n);
+int length(int n);
+int my_pow(int n, int j);
+int length(long long n);
+int my_pow(int n, int j);
 
 #endif
