7b47e185ba1a8b53fb9c7bb77bc48352fac92320
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2022-11-23 11:56:05 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,6 +1,16 @@
 
 #include "NumClass.h"
-
+//int length(int n)
+//{
+//    if (n == 0)
+//        return 1;
+//    int count = 0;
+//    while (n != 0) {
+//        n = n / 10;
+//        ++count;
+//    }
+//    return count;
+//}
 int my_pow(int n, int j) {
     int cnt = 1;
     for(int i = 0; i<j; i++){
@@ -8,48 +18,25 @@ int my_pow(int n, int j) {
     }
     return cnt;
 }
-int length(int n)
-{
-    if (n == 0)
-        return 1;
-    int count = 0;
-    while (n != 0) {
-        n = n / 10;
-        ++count;
+int rec_revers(int n){
+    int l = length(n);
+    if (l==1){
+        return n;
     }
-    return count;
+    return rec_revers(n/10)+(n%10*my_pow(10, l- 1));
 }
-int rec_fact(int n){
-    if(n==1){
-        return 1;
-    }
-    return n* rec_fact(n-1);
+int isPalindrome(int n) {
+    return ((n == rec_revers(n)) || (n >= 0 && n < 10));
 }
-int ezer(int n,int l) {
+int Armstrong(int n,int l) {
     if(n==0){
         return 0;
     }
     int lsb = n%10;
-    return my_pow(lsb,l)+ ezer(n/10,l);
+    return my_pow(lsb,l)+ Armstrong(n/10,l);
 
 }
 int isArmstrong(int n){
     int l = length(n);
-    return ezer(n,l)==n;
+    return Armstrong(n,l)==n;
 }
-int  isPalindrome(int n){
-    int newN = n;
-    if(n<10){
-        return 1;
-    }
-    int l = length(n);
-
-    int t = my_pow(10, l-1) ;
-    int i = newN%10;
-    int j = newN / t;
-    newN = newN % t;
-    newN= newN/10;
-    return (i == j) && (isPalindrome(newN));
-}
-
-
