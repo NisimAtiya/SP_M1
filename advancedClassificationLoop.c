36fae77773eb10c38ca1a87e37ac1ed80bf53eff
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2022-11-23 11:55:36 +0200

    Update advancedClassificationLoop.c

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,16 +1,4 @@
-
 #include "NumClass.h"
-int length(int n)
-{
-    if (n == 0)
-        return 1;
-    int count = 0;
-    while (n != 0) {
-        n = n / 10;
-        ++count;
-    }
-    return count;
-}
 int my_pow(int n, int j) {
     int cnt = 1;
     for(int i = 0; i<j; i++){
@@ -18,36 +6,25 @@ int my_pow(int n, int j) {
     }
     return cnt;
 }
-int  isPalindrome(int n){
-    int copy = n;
-   int i = 0;
-   int j = length(n) - 1;
-   int arr[j+1];
-    while (n>0){
-        arr[i] = copy%10;
-        copy = copy/10;
-        i++;
+int isPalindrome(int n){
+    int n_ = n;
+    int l = length(n);
+    int reverse_n=0,i;
+    for (i = 0; i < l; i++){
+        reverse_n=(reverse_n*10)+(n_ % 10);
+        n_ = n_/10;
     }
-    i=0;
-    while (i<=j){
-        if(arr[i]!=arr[j]){
-            return 0;
-        }
-        i++;
-        j--;
-    }
-    return 1;
+    return(reverse_n == n);
 }
 int isArmstrong(int n){
 
     int l = length(n);
-    int copy=n,sum=0;
+    int n_=n,sum=0;
     for(int i=0;i<l;i++){
-        int j = copy%10;
+        int j = n_%10;
         sum += my_pow(j,l);
-        copy = copy/10;
+        n_ = n_/10;
     }
     return (sum==n);
 
 }
-
